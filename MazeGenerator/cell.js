f6a2c888edb86df2e06d711655c5c3452aaf9dbb
Yusuf Duran <firatyusuf.d@gmail.com>
2018-03-24 15:25:55 +0100

    finished work

--- MazeGenerator/cell.js
+++ MazeGenerator/cell.js
@@ -4,29 +4,36 @@ class Cell {
         this.y = y * w;
         this.w = w;
         this.visited = false;
-        if (this.y === 0) {
-            this.north = true
-        } else {
-            this.north = false;
-        }
-        if (this.x + this.w === width) {
-            this.east = true
-        } else {
-            this.east = false;
-        }
-        if (this.y + this.w === height) {
-            this.south = true
-        } else {
-            this.south = false;
-        }
-        if (this.x === 0) {
-            this.west = true
-        } else {
-            this.west = false;
-        }
+        // if (this.y === 0) {
+        //     this.north = true
+        // } else {
+        //     this.north = false;
+        // }
+        // if (this.x + this.w === width) {
+        //     this.east = true
+        // } else {
+        //     this.east = false;
+        // }
+        // if (this.y + this.w === height) {
+        //     this.south = true
+        // } else {
+        //     this.south = false;
+        // }
+        // if (this.x === 0) {
+        //     this.west = true
+        // } else {
+        //     this.west = false;
+        // }
+        this.north = true
+        this.east = true
+        this.south = true
+        this.west = true
     }
     show() {
         fill(0, 0, 0);
+        if (this.visited) {
+            fill(127);
+        }
         noStroke();
         rect(this.x, this.y, this.w, this.w);
         if (this.north) {
@@ -34,21 +41,11 @@ class Cell {
             stroke(255, 0, 0);
             strokeWeight(4);
             line(this.x, this.y, this.x + this.w, this.y);
-        } else {
-            //open wall
-            stroke(0, 255, 0);
-            strokeWeight(1);
-            line(this.x, this.y, this.x + this.w, this.y);
         }
         if (this.east) {
             //closed wall
             stroke(255, 0, 0);
             strokeWeight(4);
-            line(this.x + this.w, this.y, this.x + this.w, this.y + this.w);
-        } else {
-            //open wall
-            stroke(0, 255, 0);
-            strokeWeight(1);
             line(this.x + this.w, this.y, this.x + this.w, this.y + this.w);
         }
         if (this.south) {
@@ -56,22 +53,40 @@ class Cell {
             stroke(255, 0, 0);
             strokeWeight(4);
             line(this.x, this.y + this.w, this.x + this.w, this.y + this.w);
-        } else {
-            //open wall
-            stroke(0, 255, 0);
-            strokeWeight(1);
-            line(this.x, this.y + this.w, this.x + this.w, this.y + this.w);
         }
         if (this.west) {
             //closed wall
             stroke(255, 0, 0);
             strokeWeight(4);
             line(this.x, this.y, this.x, this.y + this.w);
-        } else {
-            //open wall
-            stroke(0, 255, 0);
-            strokeWeight(1);
-            line(this.x, this.y, this.x, this.y + this.w);
         }
+    }
+    getNeighbours(cells) {
+        let neighbours = [];
+        //up
+        if (!(this.y === 0)) {
+            if (cells[this.y / this.w - 1][this.x / this.w].visited === false) {
+                neighbours.push(cells[this.y / this.w - 1][this.x / this.w]);
+            }
+        }
+        //right
+        if (!(this.x + this.w === width)) {
+            if (cells[this.y / this.w][this.x / this.w + 1].visited === false) {
+                neighbours.push(cells[this.y / this.w][this.x / this.w + 1]);
+            }
+        }
+        //down
+        if (!(this.y + this.w === height)) {
+            if (cells[this.y / this.w + 1][this.x / this.w].visited === false) {
+                neighbours.push(cells[this.y / this.w + 1][this.x / this.w]);
+            }
+        }
+        //left
+        if (!(this.x === 0)) {
+            if (cells[this.y / this.w][this.x / this.w - 1].visited === false) {
+                neighbours.push(cells[this.y / this.w][this.x / this.w - 1]);
+            }
+        }
+        return neighbours;
     }
 }
--- MazeGenerator/sketch.js
+++ MazeGenerator/sketch.js
@@ -1,6 +1,8 @@
 let w = 40;
 let col, row;
-let cells = [];
+let grid = [];
+let current;
+let stack = []
 
 function setup() {
     createCanvas(400, 400);
@@ -11,18 +13,47 @@ function setup() {
         for (let j = 0; j < row; j++) {
             temp.push(new Cell(j, i, w))
         }
-        cells.push(temp);
+        grid.push(temp);
     }
-    cells[0][0].visited = true;
+    grid[0][0].visited = true;
+    current = grid[0][0];
 }
 
 //cell[y][x]
 
 function draw() {
     background(0);
-    for (rows of cells) {
-        for (cell of rows) {
-            cell.show();
+    grid.forEach(array => array.forEach(cell => cell.show()))
+    if (grid.every(array => array.every(cell => cell.visited))) {
+        noLoop();
+    }
+    let neighbours = current.getNeighbours(grid);
+    if (neighbours.length > 0) {
+        let next = random(neighbours);
+        stack.push(current);
+        xoffset = current.x / current.w - next.x / current.w;
+        yoffset = current.y / current.w - next.y / current.w;
+        if(yoffset === 1){
+            //up
+            current.north = false;
+            next.south = false;
+        }else if (xoffset === -1) {
+            //right
+            current.east = false;
+            next.west = false;
+        }else if (yoffset === -1) {
+            //down
+            current.south = false;
+            next.north = false;
+        }else if (xoffset === 1) {
+            //left
+            current.west = false;
+            next.east = false;
         }
+        current = next;
+        current.visited = true
+    }else{
+        current = stack[stack.length - 1]
+        stack.splice(-1,1);
     }
 }
